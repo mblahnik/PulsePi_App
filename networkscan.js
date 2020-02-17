c7392b5f2150854397008f54616809a1fdd04b8b
Alex <mblahnik@uwm.edu>
2020-02-17 15:53:04 -0600

    Add comments

--- networkscan.js
+++ networkscan.js
@@ -1,6 +1,8 @@
 const find = require("local-devices");
 var net = require("net");
 
+const SERVER_PORT = 23;
+
 var arduinoMac = "a4:cf:12:85:61:10";
 var arduinoIp;
 var client = new net.Socket();
@@ -10,13 +12,20 @@ document.getElementById("test").addEventListener("click", function() {
   scan();
 });
 
-//  {name : "", ip : "", mac: ""}
+/* Set the ip of the Arduino manually */
+function setIp(ip) {
+  arduinoIp = ip;
+}
+
+/* Scan the local netwrok looking for the arduino.
+   If the arduino is found, the ip will be set */
+// devices format -> {name : "", ip : "", mac: ""}
 function scan() {
   find().then(devices => {
     devices.forEach(element => {
       if (element.mac == arduinoMac) {
         console.log("Arduino found at ip " + element.ip);
-        arduinoIp = element.ip;
+        setIp(element.ip);
       }
     });
     if (arduinoIp) connect();
@@ -24,20 +33,27 @@ function scan() {
   });
 }
 
+/* Attempt to open a client socket with the Arduino. */
 function connect() {
   console.log("trying to connect to " + arduinoIp);
-  client.connect(23, arduinoIp, function() {
+  client.connect(SERVER_PORT, arduinoIp, function() {
     console.log("Connected");
   });
 }
 
+/* Set what happens when the Arduino sends over data */
 client.on("data", function(data) {
   console.log("Received: " + data);
   //client.destroy(); // kill client after server's response
 });
 
+/* Set what happens when the socket it closed */
 client.on("close", function() {
   console.log("Connection closed");
+});
+
+client.on("error", function(err) {
+  console.log("An error has occured");
 });
 
 document.getElementById("send").addEventListener("click", function() {
