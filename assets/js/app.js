947d1490a2c1cfc3ba5ceecb15443f91a0091e44
spykim2003 <spykim2003@gmail.com>
2020-03-01 00:19:35 -0600

    new category: business -> all

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,25 +1,25 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
-  let url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=5b92f60e792e449591bd017b1453aeeb";
+    let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=5b92f60e792e449591bd017b1453aeeb";
 
-  $.ajax({
-    url: url,
-    method: "GET",
-    dataType: "JSON",
+    $.ajax({
+        url: url,
+        method: "GET",
+        dataType: "JSON",
 
-    beforeSend: function () {
-      $(".progress").show();
-    },
+        beforeSend: function() {
+            $(".progress").show();
+        },
 
-    complete: function () {
-      $(".progress").hide();
-    },
+        complete: function() {
+            $(".progress").hide();
+        },
 
-    success: function (newsdata) {
-      let output = "";
-      let latestNews = newsdata.articles;
-      for (var i in latestNews) {
-        output += `
+        success: function(newsdata) {
+            let output = "";
+            let latestNews = newsdata.articles;
+            for (var i in latestNews) {
+                output += `
           <div class="col l4 m6 s12">
           <div class="card medium hoverable">
             <div class="card-image">
@@ -44,18 +44,18 @@ $(document).ready(function () {
            </div>
           </div>
         `;
-      }
+            }
 
-      if (output !== "") {
-        $("#newsResults").html(output);
-      }
+            if (output !== "") {
+                $("#newsResults").html(output);
+            }
 
-    },
+        },
 
-    error: function () {
-      let errorMsg = `<div class="errorMsg center">Some error occured</div>`;
-      $("#newsResults").html(errorMsg);
-    }
-  })
+        error: function() {
+            let errorMsg = `<div class="errorMsg center">Some error occured</div>`;
+            $("#newsResults").html(errorMsg);
+        }
+    })
 
 });
