e900a9548ad8f65712bc6af9089e33520ab7d892
mblahnik <mblahnik@uwm.edu>
2020-04-01 04:09:04 -0500

    Form validation works

--- CreateAccount.js
+++ CreateAccount.js
@@ -1,4 +1,58 @@
-const CreateAccountURL = 'https://pulsepi.azurewebsites.net/api/';
+const $ = require('jquery');
+const CreateAccountURL =
+  'https://pulsepi.azurewebsites.net/api/account/createAccount';
+
+const required = function(formControlId) {
+  let value = $(formControlId).val();
+  if (!/\S/.test(value)) {
+    $(formControlId).addClass('is-invalid');
+    if (!$(formControlId).siblings('.required').length)
+      $(formControlId).after(
+        `<div class="text-danger required" id="1${formControlId}">Value Is Required</div>`
+      );
+  } else {
+    $(formControlId).removeClass('is-invalid');
+    if ($(formControlId).siblings('.required').length)
+      $(formControlId)
+        .siblings('.required')
+        .remove();
+  }
+};
+
+const mustMatch = function(formControlId) {
+  if ($(formControlId).val() && $('#CAPasswordInput').val()) {
+    if ($(formControlId).val() !== $('#CAPasswordInput').val())
+      $(formControlId).addClass('is-invalid');
+    if (!$(formControlId).siblings('.match').length)
+      $(formControlId).after(
+        `<div class="text-danger match" id="1${formControlId}">Passwords Must Match</div>`
+      );
+    else {
+      $(formControlId).removeClass('is-invalid');
+      if ($(formControlId).siblings('.match').length)
+        $(formControlId)
+          .siblings('.match')
+          .remove();
+    }
+  }
+};
+
+const formIdsAndValidators = {
+  '#CAUsernameInput': [required],
+  '#CAPasswordInput': [required],
+  '#CAPasswordRepeat': [required, mustMatch],
+  '#CAFirstName': [required],
+  '#CALastName': [required],
+  '#CAInputEmail': [required]
+};
+
+const verifyForm = function() {
+  for (let [key, value] of Object.entries(formIdsAndValidators))
+    value.forEach(x => x(key));
+
+  if ($('.is-invalid').length) return false;
+  else return true;
+};
 
 const CreateAccountWindow = {
   remote: require('electron'),
@@ -14,30 +68,43 @@ const CreateAccountWindow = {
     });
     win.loadFile('Register.html');
     win.setMenu(null);
+    win.webContents.openDevTools();
   },
-  init: function() {
-    $('#SubmitCreateAccountBtn').click(function() {
-      //This should verify Inputs before sending.
-      $.ajax({
-        url: CreateAccountURL,
-        type: 'POST',
-        dataType: 'json',
-        contentType: 'application/json',
-        data: JSON.stringify({
-          username: $('#CAUsernameInput').val(),
-          password: $('#CAPasswordInput').val(),
-          firstName: $('#CAFirstName').val(),
-          lastName: $('#CALastName').val(),
-          email: $('#CAInputEmail').val()
-        }),
-        success: function(data) {
-          //Something Cool should happen
-        },
-        error: function(data) {
-          //An error happened
-        }
+  setFormValidation: function() {
+    $('form').change(function(event) {
+      let id = `#${event.target.id}`;
+      formIdsAndValidators[id].forEach(validatorfn => {
+        validatorfn(id);
       });
     });
+  },
+  setSubmitListener: function() {
+    $('#SubmitCreateAccountBtn').click(function() {
+      if (verifyForm())
+        $.ajax({
+          url: CreateAccountURL,
+          type: 'POST',
+          dataType: 'json',
+          contentType: 'application/json',
+          data: JSON.stringify({
+            username: $('#CAUsernameInput').val(),
+            password: $('#CAPasswordInput').val(),
+            firstName: $('#CAFirstName').val(),
+            lastName: $('#CALastName').val(),
+            email: $('#CAInputEmail').val()
+          }),
+          success: function(data) {
+            console.log(data);
+          },
+          error: function(data) {
+            console.log(data);
+          }
+        });
+    });
+  },
+  init: function() {
+    this.setSubmitListener();
+    this.setFormValidation();
   }
 };
 
