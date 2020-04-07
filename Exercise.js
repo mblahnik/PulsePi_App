e669aa4b2d99cfbe43e7c91250068c488f04dbeb
Leuenberger <leuenbe4@uwm.edu>
2020-04-07 16:11:01 -0500

    cleaning up

--- Exercise.js
+++ Exercise.js
@@ -69,8 +69,8 @@ const ExercisePage = {
       bpm = Math.min(Math.max(bpm, 55), 120);
       //Still need to check - sometimes HRs get sent too fast and the numbers get 
       //combined as something like: "678992", we don't wanna save that 
-      if(bpm < 120 && bpm > 55) {
-        ExercisePage.data.processNewBpm(parseInt(data.toString()));
+      if(bpm < 120) {
+        ExercisePage.data.processNewBpm(bpm);
         $('#CurrentBpm').text(`${data.toString()} Bpm`);
         $('#MaxBpm').text(`${ExercisePage.data.bpmHigh} Bpm`);
         $('#MinBpm').text(`${ExercisePage.data.bpmLow} Bpm`);
